33955e88e618162b3ac638282eafd02b4612aa4a
102693146 <abdullahsiddiqui44iq@users.noreply.github.com>
2024-01-21 21:57:37 +0500

    result component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,30 @@
 import { calculateInvestmentResults } from "../util/investment";
 
-export default function Results({ userInput }){
-    return <p>Results.</p>
+export default function Results({ userInput }) {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Initial Investment</th>
+          <th>Annual Investment</th>
+          <th>Interest Earned</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {calculateInvestmentResults(userInput).map((result, index) => {
+          return (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{result.initialInvestment}</td>
+              <td>{result.annualInvestment}</td>
+              <td>{result.interestEarned}</td>
+              <td>{result.total}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
